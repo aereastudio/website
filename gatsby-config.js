a49aa951aa1c4b47ee2c3067f36b9c3a318fa3ae
dalvaradotey <d.alvaradotey@gmail.com>
2021-04-21 18:20:24 -0400

    ADD: favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,5 +24,17 @@ module.exports = {
         }
       }
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#000`,
+        theme_color: `#000`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
   ],
 };
